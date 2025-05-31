41fad552fbf5628aad2f6976cf88864caa6a6bb9
Malik <malikdemir957@gmail.com>
2025-05-31 20:38:10 +0200

    added SnakeBody + Apple + Collision + Game Over

--- SnakeFrame.java
+++ SnakeFrame.java
@@ -1,20 +1,20 @@
-import java.awt.Dimension;
 import javax.swing.JFrame;
 
 public class SnakeFrame extends JFrame {
     // Customizable
     final static String TITLE = "Snake";
-    final Dimension SIZE = new Dimension(800, 800);
-    final int LINES = 24;
+    final int LINES = 20;
+    final int TILE_SIZE = 20;
+    final int PANEL_SIZE = LINES * TILE_SIZE;
 
     public SnakeFrame() {
         super(TITLE);
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(true);
-        setPreferredSize(SIZE);
-        add(new SnakePanel(LINES));
+        add(new SnakePanel(LINES, TILE_SIZE));
         pack();
+        setLocationRelativeTo(null);
         setVisible(true);
     }
 }
--- SnakePanel.java
+++ SnakePanel.java
@@ -4,34 +4,77 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 import javax.swing.Timer;
+import java.awt.Dimension;
 
 public class SnakePanel extends JPanel implements KeyListener {
     int lines;
+    int tileSize;
 
     // Snake Head
     Tile snakeHead;
 
+    // Snake Body
+    ArrayList<Tile> snakeBody;
+
+    // Apple
+    Tile apple;
+
     // Movement
+    int bufferXVelocity = 0;
+    int bufferYVelocity = 0;
     int yVelocity = 0;
     int xVelocity = 0;
 
+    boolean isGameOver = false;
+
     Timer timer;
 
-    public SnakePanel(int lines) {
+    public SnakePanel(int lines, int tileSize) {
         super();
 
+        setPreferredSize(new Dimension(lines * tileSize, lines * tileSize));
         setBackground(Color.BLACK);
+        
         this.lines = lines;
+        this.tileSize = tileSize;
+
+        SwingUtilities.invokeLater(() -> {
+            apple = new Tile(5, 5);
+            placeApple();
+        });
 
         snakeHead = new Tile(0, 0);
-        Timer timer = new Timer(200, new ActionListener() {
+        snakeBody = new ArrayList<Tile>();
+
+        timer = new Timer(200, new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                move();
-                repaint();
+                if (isColliding(snakeHead, apple)) {
+                    snakeBody.add(new Tile(apple.x, apple.y));
+                    placeApple();
+                }
+
+                if (isOutOfBounds(snakeHead)) {
+                    isGameOver = true;
+                }
+
+                
+
+                if (!isGameOver) {
+                    move();
+                    repaint();
+                }
+
+                for (Tile bodyPart : snakeBody) {
+                    if (isColliding(snakeHead, bodyPart)) {
+                        isGameOver = true;
+                    }
+                }
             }
 
         });
@@ -44,50 +87,86 @@ public class SnakePanel extends JPanel implements KeyListener {
     }
 
     public void move() {
+        bufferXVelocity = xVelocity;
+        bufferYVelocity = yVelocity;
+
         int x = snakeHead.getX();
         int y = snakeHead.getY();
 
         snakeHead.setPosition(x + xVelocity, y + yVelocity);
     }
 
+    public void placeApple() {
+        int x = (int) (Math.random() * lines);
+        int y = (int) (Math.random() * lines);
+
+        apple.setPosition(x, y);
+    }
+
+    public boolean isColliding(Tile t1, Tile t2) {
+        return (t1.x == t2.x && t1.y == t2.y);
+    }
+
+    public boolean isOutOfBounds(Tile t) {
+        return (t.x < 0 || t.x > lines || t.y < 0 || t.y > lines);
+    }
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        int tileHeight = (int) (getHeight() / lines);
-        int tileWidth = (int) (getWidth() / lines);
+        // Draw Apple
+        g.setColor(Color.RED);
+        g.fillRect(apple.x * tileSize, apple.y * tileSize, tileSize, tileSize);
 
         // Draw Snake Head
         g.setColor(Color.GREEN);
-        g.fillRect(snakeHead.x * tileWidth, snakeHead.y * tileHeight, tileWidth, tileHeight);
+        g.fillRect(snakeHead.x * tileSize, snakeHead.y * tileSize, tileSize, tileSize);
+
+        // Draw Snake Body
+        for (int i = snakeBody.size() - 1; i > -1; i--) {
+            Tile bodyPart = snakeBody.get(i);
+
+            g.fillRect(bodyPart.x * tileSize, bodyPart.y * tileSize, tileSize, tileSize);
+
+            if (i != 0) {
+                Tile previousBodyPart = snakeBody.get(i - 1);
+
+                bodyPart.x = previousBodyPart.x;
+                bodyPart.y = previousBodyPart.y;
+            } else {
+                bodyPart.x = snakeHead.x;
+                bodyPart.y = snakeHead.y;
+            }
+        }
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
         switch (e.getKeyCode()) {
             case KeyEvent.VK_A:
-                if (xVelocity == 0) {
+                if (bufferXVelocity == 0) {
                     xVelocity = -1;
                     yVelocity = 0;
                 }
                 break;
 
             case KeyEvent.VK_D:
-                if (xVelocity == 0) {
+                if (bufferXVelocity == 0) {
                     xVelocity = 1;
                     yVelocity = 0;
                 }
                 break;
 
             case KeyEvent.VK_W:
-                if (yVelocity == 0) {
+                if (bufferYVelocity == 0) {
                     xVelocity = 0;
                     yVelocity = -1;
                 }
                 break;
 
             case KeyEvent.VK_S:
-                if (yVelocity == 0) {
+                if (bufferYVelocity == 0) {
                     xVelocity = 0;
                     yVelocity = 1;
                 }
